c46fc6b234924a3cf81cd78a47e4207070ce35f6
Gozman <thegzmn@gmail.com>
2022-11-15 00:41:39 +0100

    Update logic

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const sidebar = document.querySelector('aside');
 openSidebar.addEventListener('click', showSidebar);
 
 function showSidebar() {
-    sidebar.style.display = 'block';
+    sidebar.style.transform = 'translateY(0)';
 }
 
-const closeSidebarbtn = document.querySelector('aside li:has(i.fa-close)');
+const closeSidebarbtn = document.querySelector('#close-aside');
 
 closeSidebarbtn.addEventListener('click', closeSidebar);
 
 function closeSidebar() {
-    sidebar.style.display = 'none';
+    sidebar.style.transform = 'translateY(-100%)';
 }
